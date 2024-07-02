36d0e9b02f020f62939322c4f6b81495036c2d7b
gongseunghwa <tmdghk502@naver.com>
2024-07-02 14:44:49 +0900

    transactional 어노테이션 활용

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -4,12 +4,15 @@ import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import lombok.*;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @RequiredArgsConstructor
 @Service
 public class StockService {
     private final StockRepository stockRepository;
+
+    @Transactional
     public void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findStockByProductId(id).orElseThrow();
         stock.decrease(quantity);
